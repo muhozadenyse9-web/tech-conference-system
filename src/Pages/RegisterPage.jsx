898202829c122359d8172843aa053c6d2cda5576
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-03-10 12:56:52 +0200

    added a simple placeholder page for registration

---
+++ src/Pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+function RegisterPage() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">Registration Form Page</h1>
+    </div>
+  )
+}
+
+export default RegisterPage
