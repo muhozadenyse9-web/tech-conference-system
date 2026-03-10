e67ffa1376896be7685b2dda840a3f50541aef25
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-03-10 13:00:33 +0200

    added a small first step confirmation for users

---
+++ src/Pages/ConfirmationPage.jsx
@@ -0,0 +1,11 @@
+function ConfirmationPage() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold text-green-600">
+        Registration Confirmed
+      </h1>
+    </div>
+  )
+}
+
+export default ConfirmationPage
