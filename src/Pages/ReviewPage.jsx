e19e96faf0b2c2b52117ce3d61a03ec7a4a42e29
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-03-10 12:58:29 +0200

    added a simple checkout page for system review

---
+++ src/Pages/ReviewPage.jsx
@@ -0,0 +1,9 @@
+function ReviewPage() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">Review Registration</h1>
+    </div>
+  )
+}
+
+export default ReviewPage
