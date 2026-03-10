053749a633f9ede6e3670445d9926633dda14250
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-03-10 13:03:48 +0200

    created a final page confirming to the user that he/she has already filled out the form

---
+++ src/Pages/SubmittedPage.jsx
@@ -0,0 +1,11 @@
+function SubmittedPage() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">
+        You Have Already Registered
+      </h1>
+    </div>
+  )
+}
+
+export default SubmittedPage
